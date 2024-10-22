bfd96c9cc43e3d236971d3412b75b22934760b29
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-10-22 17:47:57 +0530

    h

---
+++ head.js
@@ -0,0 +1,9 @@
+// console.log(1)
+// now = new Date();
+// day = String(now.getUTCDate()).padStart(2, "0");
+// month = String(now.getUTCMonth() + 1).padStart(2, "0");
+// year = String(now.getUTCFullYear()).slice(-2);
+// hour = String(now.getUTCHours()).padStart(2, "0");
+
+// queryString = `cachebuster=${day}${month}${year}${hour}`;
+kat = 0
