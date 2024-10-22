39e0ca0fef576da492282af5cdb23bdf3ef3d49b
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-10-22 17:17:01 +0530

    e

--- hn.js
+++ hn.js
@@ -40,7 +40,15 @@ fetch(`https://cdn.hnup.date/generated_audio.mp3?${queryString}`, {
 //   mediaControls: false,
 // };
 
-// wavesurfer = WaveSurfer.create(options);
+wavesurfere = WaveSurfer.create({...options, plugins: [
+  Hover.create({
+    lineColor: '#ff0000',
+    lineWidth: 2,
+    labelBackground: '#555',
+    labelColor: '#fff',
+    labelSize: '11px',
+  }),
+]});
 
 // wavesurfer.on("ready", (duration) => {
 //   const loader = document.getElementById("loader");
